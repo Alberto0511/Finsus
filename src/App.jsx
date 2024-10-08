684e8059c0e2c8d5d5b1557d3f3a64747fd9963d
KrisMG0 <cristopher.omg@gmail.com>
2024-10-08 09:54:53 -0600

    PASCU

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import IntenerestCalculator from './components/IntenerestCalculator';
+import SelectInteres from './components/SelectInteres';
+
+
 function App() {
   return (
     <>
       <IntenerestCalculator />
+      <SelectInteres/>
     </>
+
+    
   );
 }
 
 export default App;
+
