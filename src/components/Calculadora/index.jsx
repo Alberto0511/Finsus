82b6db2a83829bda7e0c77eaac6c76fc3a7fddc9
KrisMG0 <cristopher.omg@gmail.com>
2024-10-28 16:06:10 -0600

    Calculadora

--- src/components/Calculadora/index.jsx
+++ src/components/Calculadora/index.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect } from "react";
-import { InputNumber } from "antd";
+import { InputNumber,Select } from "antd";
 import { SetNewListGroups } from "./Header";
-import "./index.css";
+import dataReinversion from "./catalog";
+import {
+  MainDiv,
+  InvestmentHeader ,
+  HeaderTable ,
+  TableInversion,
+  SubtitleTable,
+} from "./index.styles";
+
+
+// import "./index.css";
 
 function Calculadora() {
   const [valueInversion, seValueInversion] = React.useState(100);
@@ -12,101 +22,7 @@ function Calculadora() {
     { label: "Plazo", key: "Plazo" },
     { label: "Rendimiento", key: "Rendimiento" },
   ]);
-  const [dataReinversion] = React.useState([
-    {
-      Tasa: "10.06%",
-      Plazo: "7 dias",
-      Rendimiento: "",
-      key: 7,
-      gatR: "6.56%",
-      gatN: "10.70%",
-      ClassName: `row seven`,
-    },
-    {
-      Tasa: "11.09%",
-      Plazo: "1 mes",
-      Rendimiento: "",
-      key: 30,
-      gatR: "7.50%",
-      gatN: "11.67%",
-      ClassName: `row twenty-eight`,
-    },
-    {
-      Tasa: "12.09% ",
-      Plazo: "3 meses",
-      Rendimiento: "",
-      key: 90,
-      gatR: "8.44%",
-      gatN: "12.65%",
-      ClassName: `row ninety`,
-    },
-    {
-      Tasa: "12.29% ",
-      Plazo: "6 meses",
-      Rendimiento: "",
-      key: 180,
-      gatR: "8.46%",
-      gatN: "12.67%",
-      ClassName: `row three-sixty-five`,
-    },
-    {
-      Tasa: "14.09%",
-      Plazo: "12 meses",
-      Rendimiento: "",
-      key: 360,
-      morePopular: false,
-      gatR: "9.83%",
-      gatN: "14.09%",
-      ClassName: `row three-sixty-six`,
-    },
-    // {
-    //   Tasa: "16.00%",
-    //   Plazo: "18 meses",
-    //   Rendimiento: "",
-    //   key: 540,
-    //   morePopular: false,
-    //   gatR: "11.67%",
-    //   gatN: "16.00%",
-    //   ClassName: `row three-sixty-six2`
-    // },
-    {
-      Tasa: "14.55%",
-      Plazo: "2 años",
-      Rendimiento: "",
-      key: 720,
-      gatR: "10.27%",
-      gatN: "14.55%",
-      ClassName: `row three-sixty-seven`,
-    },
-    {
-      Tasa: "15.01%",
-      Plazo: "3 años",
-      Rendimiento: "",
-      key: 1080,
-      gatR: "10.71%",
-      gatN: "15.01%",
-      ClassName: `row three-sixty-eight`,
-    },
-    {
-      Tasa: "13.25%",
-      Plazo: "4 años",
-      Rendimiento: "",
-      key: 1440,
-      gatR: "9.02%",
-      gatN: "13.25%",
-      ClassName: `row three-sixty-nine`,
-    },
-    {
-      Tasa: "12.75%",
-      Plazo: "5 años",
-      Rendimiento: "",
-      key: 1800,
-      gatR: "8.54%",
-      gatN: "12.75%",
-      ClassName: "row three-sixty-ten",
-    },
-  ]);
-
+  
   const [tableSelect, setTableSelect] = React.useState([]);
 
   useEffect(() => {
@@ -259,51 +175,53 @@ function Calculadora() {
   };
 
   return (
-    <>
-      <div className="investment-simulator">
-        <div className="investment-header">
-          <h3 className="header">Simula tu inversión</h3>
-          <p className="investment-description">
-            Escribe una cantidad y descubre su rendimiento según el plazo.
-          </p>
-          <InputNumber
-            id="inputNumberAnt"
-            controls={false}
-            className="input-number"
-            min={100}
-            value={valueInversion}
-            defaultValue={100}
-            formatter={(value) =>
-              `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            }
-            parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
-            onChange={onChange}
-          />
-          <span className="minimum-amount">El monto mínimo es de $100.00</span>
-        </div>
-        <div className="investment-details">
-          <div className="investment-online-status">{onlines()}</div>
-          <div className="investment-results">
-            {dataReinversion.map((data, i) => (
-              <div
-                key={i}
-                className={`investment-item ${isActive ? "active" : ""} ${
-                  data.ClassName
-                }`}
-              >
-                <div className="investment-period">{data.Plazo}</div>
-                <div className="investment-yield" id="prime">
-                  <span title={data.Rendimiento}>{data.Rendimiento}</span>
+
+    <MainDiv>
+      <TableInversion className="investment-simulator">
+        <InvestmentHeader className="investment-header">
+          
+            <HeaderTable>Simula tu inversión</HeaderTable>
+            <SubtitleTable className="investment-description">
+              Escribe una cantidad y descubre su rendimiento según el plazo.
+            </SubtitleTable>
+            <InputNumber
+              id="inputNumberAnt"
+              controls={false}
+              className="input-number"
+              min={100}
+              value={valueInversion}
+              defaultValue={100}
+              formatter={(value) =>
+                `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              }
+              parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
+              onChange={onChange}
+            />
+            <span className="minimum-amount">El monto mínimo es de $100.00</span>
+          </InvestmentHeader>
+          <div className="investment-details">
+            <div className="investment-online-status">{onlines()}</div>
+            <div className="investment-results">
+              {dataReinversion.map((data, i) => (
+                <div
+                  key={i}
+                  className={`investment-item ${isActive ? "active" : ""} ${
+                    data.ClassName
+                  }`}
+                >
+                  <div className="investment-period">{data.Plazo}</div>
+                  <div className="investment-yield" id="prime">
+                    <span title={data.Rendimiento}>{data.Rendimiento}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <span className="hidden-text"></span>
+            {getGat("GAT REAL", "gatR")}
+            {getGat("GAT NOMINAL", "gatN")}
           </div>
-          <span className="hidden-text"></span>
-          {getGat("GAT REAL", "gatR")}
-          {getGat("GAT NOMINAL", "gatN")}
-        </div>
-      </div>
-    </>
+      </TableInversion>
+    </MainDiv>
   );
 }
 
---
+++ src/components/Calculadora/index.styles.jsx
@@ -0,0 +1,175 @@
+import styled from "styled-components";
+import { InputNumber, Select } from "antd";
+
+
+
+export const MainDiv = styled.div`
+ background: #23223f; 
+  min-height: 100vh; 
+  width: 100vw; 
+  margin: 0; 
+  padding: 0; 
+  box-sizing: border-box; 
+
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center; 
+
+  position: absolute; 
+  top: 0; 
+  left: 0; 
+  
+`;
+
+export const InvestmentHeader = styled.div`
+    margin-bottom: 1em;
+    
+`;
+
+export const HeaderTable = styled.h3`
+color: #fff;
+text-align: center; 
+    
+`;
+
+export const TableInversion = styled.div`
+ color: #fff; /* Color blanco */
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: 1em; /* Espaciado interno */
+  border-radius: 5px; /* Bordes redondeados */
+  border: 2px solid #fff; /* Borde blanco */
+    
+`;
+
+export const SubtitleTable = styled.p`
+color: #fff; /* Color blanco */
+margin-bottom: 0.5em; /* Margen inferior */
+text-align: center; /* Alineación centrada */
+`;
+
+
+
+
+// export const Body = styled.body`
+//   background-color: #23223f;
+//   color: white;
+//   font-family: Arial, sans-serif;
+// `;
+
+// export const InvestmentSimulator = styled.div`
+//   max-width: 1000px;
+//   margin: 2em auto;
+//   padding: 1em;
+//   border-radius: 10px;
+//   background-color: rgba(255, 255, 255, 0.2);
+//   backdrop-filter: blur(1px);
+//   border: 2px solid rgba(255, 255, 255, 0.8);
+// `;
+
+// export const Header = styled.h1`
+//   color: #fff;
+//   text-align: center;
+// `;
+
+// export const InvestmentHeader = styled.div`
+//   margin-bottom: 1em;
+// `;
+
+// export const InvestmentDescription = styled.p`
+//   color: #fff;
+//   margin-bottom: 0.5em;
+//   text-align: center;
+// `;
+
+// export const InputNumber = styled.input`
+//   border-radius: 6px;
+//   background: transparent;
+//   color: #fff;
+//   margin-bottom: 1em;
+//   padding: 0.5em;
+//   border: 1px solid #fff;
+//   width: 100%;
+// `;
+
+// export const MinimumAmount = styled.span`
+//   display: block;
+//   color: #fff;
+//   font-size: 0.9em;
+//   font-weight: bold;
+//   text-align: center;
+// `;
+
+// export const MarginBottom = styled.div`
+//   margin-bottom: 1em;
+// `;
+
+// export const InvestmentDetails = styled.div`
+//   margin-top: 1em;
+// `;
+
+// export const InvestmentOnlineStatus = styled.div`
+//   margin-bottom: 1em;
+//   color: #fff;
+//   font-weight: bold;
+// `;
+
+// export const InvestmentResults = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// export const InvestmentItem = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 0.5em;
+//   border: 1px solid #444;
+//   border-radius: 6px;
+//   margin-bottom: 0.5em;
+//   background-color: #444;
+//   transition: background-color 0.3s;
+
+//   &.active {
+//     background-color: #ff5f54;
+//   }
+// `;
+
+// export const InvestmentPeriod = styled.div`
+//   color: #fff;
+// `;
+
+// export const InvestmentYield = styled.div`
+//   color: #fff;
+// `;
+
+// export const HiddenText = styled.span`
+//   color: transparent;
+// `;
+
+// export const GatContainer = styled.div`
+//   margin-top: 2em;
+//   margin-left: -3em;
+//   width: 120%; 
+//   display: flex; 
+//   align-items: center; 
+// `;
+
+// export const Title = styled.div`
+//   margin-bottom: 0; 
+//   font-weight: bold; 
+//   text-align: left; 
+//   flex: 1; 
+// `;
+
+// export const GatItems = styled.div`
+//   display: flex;
+//   flex-wrap: wrap; 
+//   justify-content: flex-start; 
+//   margin-left: 0.5em; 
+// `;
+
+// export const GatItem = styled.div`
+//   margin: 0.3em;
+//   flex: 0 1 auto; 
+//   font-weight: bold; 
+// `;
